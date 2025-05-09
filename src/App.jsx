d9bf5f4cfc0f13343b2ade767a9ed74e6cf1bde7
Hassaan200 <hassaankhaliq45@gmail.com>
2025-05-09 18:51:52 +0500

    enable modal

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,41 @@ function App() {
     AOS.init({ duration: 1000, once: true }); 
   }, []);
 
+const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [isOpen]);
+
   return (
     <>
+
+ {/* Modal Section starts here */}
+
+{isOpen && (
+        <div className="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm bg-opacity-50 flex justify-center items-center text-white transition-opacity duration-300 ease-in-out">
+          {/* Modal content */}
+          <div className="bg-gradient-to-r from-green-800 to-green-950 text-white p-6 rounded-lg w-full max-w-md mx-4 shadow-lg relative">
+            <h2 className="text-xl font-bold mb-4">👋 Get to Know Me</h2>
+            <p className="text-white font-medium">
+              I'm a frontend developer passionate about building beautiful and functional user experiences with React, Tailwind CSS, and JavaScript. I love solving problems, learning new things, and turning ideas into real apps.
+            </p>
+            <button
+              onClick={() => setIsOpen(false)}
+              className="mt-6 bg-gray-200 text-black px-3 py-1 rounded hover:bg-gray-400 transition-all cursor-pointer font-medium duration-300"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+      {/* Modal Section ends here */}
+
+
 
     <MobileNavbar/>
       <Navbar/>
@@ -30,7 +63,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/Contact" element={<Contact/>} />
       <Route path="/Projects" element={<Projects/>} />
-      <Route path="/Services" element={<Services/>} />
+      <Route path="/Services" element={<Services setIsOpen={setIsOpen} />} />
     </Routes>
     <Footer/>
     <Toaster
